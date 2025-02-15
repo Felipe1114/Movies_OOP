6bcb1ef629cba2a6ff2549c1eb5bbe81bcb53c4a
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-02-15 16:01:20 +0100

    create website in movie_main.py eingebaut

--- movie_main.py
+++ movie_main.py
@@ -1,11 +1,14 @@
 from programm_modules.ismenu import IsMenu
 from programm_modules.movie_app import MovieApp
 from programm_modules.omdb_api import OmdbApi
+from programm_modules.createwebsite import MovieWebsiteGenerator
+from programm_modules.storage_json import StorageJson
 
 def main():
   api_key = "70e6d1f0"
   omdbapi = OmdbApi(api_key)
-  applikation = MovieApp(omdbapi,"./programm_storage/movies.json", 'json', )
+  web_generator = MovieWebsiteGenerator()
+  applikation = MovieApp(omdbapi,"./programm_storage/movies.json", web_generator, 'json')
   menu = IsMenu(applikation)
 
   menu.run()
